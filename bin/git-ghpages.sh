fa74dab577e18d420e2ba75924d07479b45fe40b
empo <e.141592@gmail.com>
2011-08-22 21:18:56 +0200

    Update project

--- bin/git-ghpages.sh
+++ bin/git-ghpages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 CO=`git checkout gh-pages`
 if [ "$?" -eq "0" ]; then
-  git checkout master
   git add .
   git commit -a -m 'Update project'
-  git push origin master  echo "Worked!"
-  exit 0
+  git push origin gh-pages
+	git checkout master	
+	exit 0
 fi
 echo "Can't update gh-pages! Error code: $?"
