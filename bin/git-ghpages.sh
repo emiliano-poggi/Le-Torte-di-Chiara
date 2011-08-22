dc868193bb1cfac3efb662ef655b6ff31aa53015
empo <e.141592@gmail.com>
2011-08-22 20:33:06 +0200

    Update project

--- bin/git-ghpages.sh
+++ bin/git-ghpages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git checkout gh-pages
-cp -r ../../Le-Torte-di-Chiara-gh-pages/* ./
+cp -r ../Le-Torte-di-Chiara-output-ghpages/* ./
 git add .
 git commit -a -m 'Update ghpages'
 git push origin gh-pages
