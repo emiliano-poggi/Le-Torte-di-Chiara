3e94a3cb7d567394f04abea200ceb3019b47ffe1
empo <e.141592@gmail.com>
2011-08-22 22:13:33 +0200

    Update project

--- bin/git-ghpages.sh
+++ bin/git-ghpages.sh
@@ -2,14 +2,15 @@
 CO=`git checkout gh-pages`
 if [ "$?" -eq "0" ]; then
   rm -rf ./*
-	echo "Copying files from output gh-pages..."
+	echo "\n>> Copying files from output gh-pages..."
 	cp -r ../Le-Torte-di-Chiara-output-ghpages/* ./
 	git status
-	echo "Synch repo..."
+	echo "\n>> Synch repo..."
 	git add .
   git commit -a -m 'Update project'
   git push origin gh-pages
-	git checkout master	
+	git checkout master
+	git status
 	exit 0
 fi
 echo "Can't update gh-pages! Error code: $?"
