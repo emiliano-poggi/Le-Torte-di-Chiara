ba68762b3438c83072c03b62fe8cb6701e6fb58e
empo <e.141592@gmail.com>
2011-08-22 16:37:29 +0200

    First commit

---
+++ output/sources/round_elements.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    var settings = {
+      tl: { radius: 20 },
+      tr: { radius: 20 },
+      bl: { radius: 20 },
+      br: { radius: 20 },
+      antiAlias: true
+    }
+	curvyCorners(settings,".contentBox");
+  }
